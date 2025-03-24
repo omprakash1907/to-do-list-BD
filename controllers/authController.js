0c54646a5b02f29cce81ea0e61f001790e87e4ed
omprakash1907 <omprakashkjat19@gmail.com>
2025-03-24 17:48:41 +0530

    first commit

---
+++ controllers/authController.js
@@ -0,0 +1,68 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+// Generate JWT Token
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" });
+};
+
+// @desc Register User
+// @route POST /auth/register
+const registerUser = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    return res.status(400).json({ message: "User already exists" });
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const user = await User.create({ name, email, password: hashedPassword });
+
+  if (user) {
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user.id),
+    });
+  } else {
+    res.status(400).json({ message: "Invalid user data" });
+  }
+};
+
+// @desc Login User
+// @route POST /auth/login
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user.id),
+    });
+  } else {
+    res.status(400).json({ message: "Invalid credentials" });
+  }
+};
+
+// @desc Get User Data
+// @route GET /auth/me
+const getMe = async (req, res) => {
+  const user = await User.findById(req.user.id).select("-password");
+  res.json(user);
+};
+
+module.exports = { registerUser, loginUser, getMe };
---
+++ server.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+
+
+const app = express();
+
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
+app.use('/auth', authRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Real-Time To-Do App Backend is Running!");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port https://localhost:${PORT}`);
+  connectDB()
+});
